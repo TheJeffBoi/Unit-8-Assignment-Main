a10b4572a3b723010bfc4471dc8c6617761d0c56
TheJeffBoi <551442@ESCG.AC.UK>
2025-05-08 14:19:19 +0100

    Level Teleport

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -1,12 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Checkpoints : MonoBehaviour
 {
+    public GameObject player;
+    public Animator transition;
+
+    public float transitionTime;
+    public float transitionDelay;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             print("Teleport Player");
+            StartCoroutine(sceneChange());
         }
     }
+
+    IEnumerator sceneChange()
+    {
+        yield return new WaitForSeconds(transitionDelay);
+
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        //player.transform.position = new Vector3(17.5f, 1, 12.1f);
+
+    }
+
 }
